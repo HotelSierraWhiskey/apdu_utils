1c0a6e773b568a8999abd4fb2194f98f2c513cec
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-11-25 12:29:17 -0500

    update credit/ debit docstrings

--- data_commands.py
+++ data_commands.py
@@ -1,11 +1,11 @@
 def command_read_data(fileno: list, offset: list, length: list) -> list:
-    '''
+    """
     BD: Read Data
     Read data from a file.
     The response will have MAC or be encrypted depending on file comms mode.
 
     NOTE: This function expects the byte order for both `length` and `offset` to be reversed.
-    '''
+    """
     apdu = [0x90, 0xBD, 0x00, 0x00]
     lc = [0x07]
     apdu += lc
@@ -18,16 +18,16 @@ def command_read_data(fileno: list, offset: list, length: list) -> list:
 
 
 def command_write_data(fileno: list, offset: list, length: list, data: list) -> list:
-    '''
+    """
     3D: Write Data
     Write data to a file.
     This needs to be sent using plain, CMAC or encrypted (with CRC) depending on file comms
-    mode. 
+    mode.
 
     NOTE: Details in RevK's manual, page 14.
     NOTE: currently this command only supports writing plain data.
     NOTE: This function expects the byte order for both `length` and `offset` to be reversed.
-    '''
+    """
     apdu = [0x90, 0x3D, 0x00, 0x00]
     lc = [len(fileno) + len(offset) + len(length) + len(data)]
     apdu += lc
@@ -41,10 +41,10 @@ def command_write_data(fileno: list, offset: list, length: list, data: list) -> 
 
 
 def command_get_value(fileno: list) -> list:
-    '''
+    """
     6C: Get Value
     Get value from a value file.
-    '''
+    """
     apdu = [0x90, 0x6C, 0x00, 0x00]
     lc = [0x01]
     apdu += lc
@@ -55,10 +55,11 @@ def command_get_value(fileno: list) -> list:
 
 
 def command_credit(fileno: list, amount: list) -> list:
-    '''
+    """
     OC: Credit
     Credit value in a value file. You need to commit the changes.
-    '''
+    amount is 4 bytes
+    """
     apdu = [0x90, 0x0C, 0x00, 0x00]
     lc = [0x05]
     apdu += lc
@@ -70,10 +71,11 @@ def command_credit(fileno: list, amount: list) -> list:
 
 
 def command_debit(fileno: list, amount: list) -> list:
-    '''
+    """
     DC: Debit
     Debit the value in a file. You need to commit the changes.
-    '''
+    amount is 4 bytes
+    """
     apdu = [0x90, 0xDC, 0x00, 0x00]
     lc = [0x05]
     apdu += lc
@@ -89,14 +91,14 @@ def command_limited_credit() -> list:
 
 
 def command_write_record(fileno: list, offset: list, length: list, data: list) -> list:
-    '''
+    """
     3B: Write Record
-    Write to a record. The offset/length are within the record. 
-    A new record is created with 00 bytes where not written. 
+    Write to a record. The offset/length are within the record.
+    A new record is created with 00 bytes where not written.
     You need to commit the changes.
 
     NOTE: This version of the command does not support data encryption
-    '''
+    """
     apdu = [0x90, 0x3B, 0x00, 0x00]
     lc = [len(fileno) + len(offset) + len(length) + len(data)]
     apdu += lc
@@ -109,11 +111,13 @@ def command_write_record(fileno: list, offset: list, length: list, data: list) -
     return apdu
 
 
-def command_read_record(fileno: list, record_number: list, number_of_records: list) -> list:
-    '''
+def command_read_record(
+    fileno: list, record_number: list, number_of_records: list
+) -> list:
+    """
     BB: Read Records
     Read records - record 0 is the oldest/first.
-    '''
+    """
     apdu = [0x90, 0xBB, 0x00, 0x00]
     lc = [0x07]
     apdu += lc
@@ -135,12 +139,11 @@ def command_clear_record_file(fileno: list) -> list:
     return apdu
 
 
-
 def command_commit_transaction() -> list:
-    '''
+    """
     C7: Commit Transaction
     Commit updates to value and record files.
-    '''
+    """
     apdu = [0x90, 0xC7, 0x00, 0x00]
     le = [0x00]
     apdu += le
